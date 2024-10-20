f2eba04575e491399b3a510ab3b88b8aa41267af
tolbist4@gmail.com <AnatoliyStavskiy>
2024-10-20 13:49:45 +0300

    add method Open in class

--- src/by/itacademy/anatoliystavskiy/numbers/pages/BasePage.java
+++ src/by/itacademy/anatoliystavskiy/numbers/pages/BasePage.java
@@ -2,4 +2,8 @@ package by.itacademy.anatoliystavskiy.numbers.pages;
 
 public class BasePage {
     protected String BASE_URL = "BASE URL";
+
+    public void open(){
+        System.out.println("Пользователь открыл страницу");
+    }
 }
