6ca331ac4aef0e321248dc57d38f3a215a40b0a1
tolbist4@gmail.com <AnatoliyStavskiy>
2024-10-22 22:10:25 +0300

    add Test1 WebElement:name

---
+++ src/test/java/biz/svyatoslav/CalculatorTest.java
@@ -0,0 +1,19 @@
+package biz.svyatoslav;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CalculatorTest {
+    @Test
+    public void test1(){
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.get("https://svyatoslav.biz/testlab/wt/index.php");
+        String inputNameXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[2]/td[2]/input";
+        By inputNameBy = By.xpath(inputNameXpath);
+        WebElement inputNameWebElement = webDriver.findElement(inputNameBy);
+        inputNameWebElement.sendKeys("John");
+    }
+}
